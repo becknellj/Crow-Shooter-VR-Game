7a903ff3552a5601c85715affa83bcdbf3a0b940
becknellj <63380733+becknellj@users.noreply.github.com>
2021-05-18 23:46:24 -0500

    Update HighScoreScript.cs

--- VR_GAME_SE4910_github/Assets/Scripts/HighScoreScript.cs
+++ VR_GAME_SE4910_github/Assets/Scripts/HighScoreScript.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+//Dinesh Punni tutorial
 
 public class HighScoreScript : MonoBehaviour
 {
     public TextMeshPro score;
     public TextMeshPro rank;
 
+    //sets the UI to the high score values
     public void setScore(string rank1, string score)
     {
         //setting the visual text
